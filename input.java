da078c89f32041153094c3b4e315f3faee78d827
thiagoraquel <thiaguinho2005@yahoo.com.br>
2026-03-27 21:00:16 -0300

    Arquivo de teste de input

---
+++ input.java
@@ -0,0 +1,30 @@
+/* * Bem-vindo à Dungeon Diagonal. 
+ * Este é um comentário de bloco longo
+ * que deve ser totalmente removido.
+ */
+
+class Heroi {
+    // Atributos do personagem
+    int nivel = 5; 
+    String nome = "Aranya"; // Comentário de fim de linha
+
+    /* Comentário de uma linha só em bloco */
+    public void atacar() {
+        int danoBase = 10;
+        
+        // O pré-processador deve manter o espaço entre 'int' e 'total'
+        // Mas pode remover espaços ao redor do '+' e '='
+        int total = danoBase + nivel * 2; 
+        
+        if (total > 15) {
+            /* O multiplicador abaixo não deve sumir, 
+            mesmo que tenha um / por perto */
+            int critico = total * 2; // total*2;
+            System.out.println("Ataque Crítico!");
+        }
+    }
+}
+
+/** * Teste de borda: / no meio de comentário // 
+ * E um asterisco isolado *
+ */
